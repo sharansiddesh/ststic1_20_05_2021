76456cdb6f39a79739808d95a666f2179602a9cf
sharansiddesh <sharansiddesh99@gmail.com>
2021-05-24 18:46:42 +0530

    first

---
+++ app1/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+from app1 import views
+
+urlpatterns = [
+    path('page1/',views.page1,name='page1'),
+    path('page2/',views.page2,name='page2'),
+    path('page3/',views.page3,name='page3'),
+    
+]
